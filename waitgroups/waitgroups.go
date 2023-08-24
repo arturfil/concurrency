c38a93b514115ad5f15a24c330e611cc435b1801
arturfil <arturfil@hotmail.com>
2023-08-23 22:04:09 -0400

    added final wait groups new code

--- waitgroups/waitgroups.go
+++ waitgroups/waitgroups.go
@@ -21,12 +21,16 @@ func WaitGroupsMain() {
         "https://udemy.com",
     }
 
-    for _, url := range urls {
+    /*for _, url := range urls {
         go getStatus(&wg, url)
         wg.Add(1)
     }
 
     wg.Wait() // wait for other waitgroups
+    */
+
+    getStatusV1(&wg, urls)
+    
 	fmt.Println("finished my go program")
 }
 
@@ -34,6 +38,23 @@ func myFunc(wg *sync.WaitGroup) {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Finished executing go-routine")
     wg.Done()
+}
+
+func getStatusV1(wg *sync.WaitGroup, urls []string) {
+    for _, url := range urls {
+        wg.Add(1)
+        go func(url string) {
+            res, err := http.Get(url)
+
+            if err != nil {
+                fmt.Printf("[Error: %s x -> %s]\n", err, url)
+            } else {
+                fmt.Printf("[%d %s]\n", res.StatusCode, url) 
+            }
+            wg.Done()
+        }(url)
+    } 
+    wg.Wait()
 }
 
 func getStatus(wg *sync.WaitGroup, url string) {
